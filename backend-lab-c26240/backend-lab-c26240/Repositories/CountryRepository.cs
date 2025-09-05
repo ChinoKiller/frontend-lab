85d196589051e230e6b894661c2814071c161076
kevin.ramirezbarboza <kevin.ramirezbarboza>
2025-09-04 21:28:05 -0600

    Laboratorio 4 de backend Kevin Ramirez Barboza C26240

---
+++ backend-lab-c26240/backend-lab-c26240/Repositories/CountryRepository.cs
@@ -0,0 +1,24 @@
+﻿using backend_lab_c26240.Models;
+using Dapper;
+using System.Data.SqlClient;
+
+namespace backend_lab_c26240.Repositories
+{
+    public class CountryRepository
+    {
+        private readonly string _connectionString;
+
+        public CountryRepository()
+        {
+            var builder = WebApplication.CreateBuilder();
+            _connectionString = builder.Configuration.GetConnectionString("CountryContext")!;
+        }
+
+        public List<CountryModel> GetCountries()
+        {
+            using var connection = new SqlConnection(_connectionString);
+            const string query = "SELECT * FROM dbo.Country";
+            return connection.Query<CountryModel>(query).ToList();
+        }
+    }
+}
---
+++ backend-lab-c26240/backend-lab-c26240/Services/CountryService.cs
@@ -0,0 +1,20 @@
+﻿using backend_lab_c26240.Models;
+using backend_lab_c26240.Repositories;
+
+namespace backend_lab_c26240.Services
+{
+    public class CountryService
+    {
+        private readonly CountryRepository countryRepository;
+
+        public CountryService()
+        {
+            countryRepository = new CountryRepository();
+        }
+
+        public List<CountryModel> GetCountries()
+        {
+            return countryRepository.GetCountries();
+        }
+    }
+}
